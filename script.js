473d2527b2024e42ffc321cd2481b7ac5656e2cf
Azdgari <109515968+Azdgari@users.noreply.github.com>
2023-06-22 11:38:41 -0500

    Add saving to local storage

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ const newTodo = document.getElementById('new-todo');
 const submitForm = document.querySelector('[data-new-todo-input]');
 const form = document.getElementById('create-new');
 
-const todos = [];
+let LOCAL_STORAGE_KEY = 'todo.listItems';
+let listItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(listItems));
+}
+
+function saveAndRender() {
+  save();
+  render();
+}
+
+let todos = [];
 
 function addNewTodo() {
   const inputValue = newTodo.value;
@@ -24,6 +36,7 @@ newTodoForm.addEventListener('submit', (e) => {
   todos.push(item);
   render();
   newTodo.value = '';
+  saveAndRender();
 });
 
 function clearElement(element) {
@@ -54,6 +67,7 @@ function render() {
     const removeBtn = document.createElement('button');
     removeBtn.id = `remove-${item.id}`;
     removeBtn.classList.add('remove-btn');
+    removeBtn.addEventListener('click', () => removeItem(item.id));
     const removeBtnImg = document.createElement('img');
     removeBtnImg.src = './images/icon-cross.svg';
     removeBtnImg.alt = 'remove item';
@@ -68,48 +82,3 @@ function render() {
 }
 
 render();
-
-//  function createNewTodo() {
-//   const item = {
-//     id: todos.length + 1,
-//     text: '',
-//     completed: false,
-//   };
-
-//   todos.unshift(item);
-
-//   const { listItem, listItemContent } = createTodoElement(item);
-// }
-
-// function createTodoElement(item) {
-//   const listItem = document.createElement('li');
-//   const listItemContent = document.createElement('div');
-//   listItemContent.type = 'text';
-//   listItemContent.value = item.text;
-//   const removeBtn = document.createElement('button');
-//   const removeBtnImg = document.createElement('img');
-
-//   listItem.classList.add('list-item');
-//   listItemContent.classList.add('list-item-content');
-//   removeBtn.classList.add('remove-btn');
-//   removeBtnImg.classList.add('remove-btn-img');
-
-//   listItemContent.textContent = item.text;
-//   removeBtnImg.src = './images/icon-cross.svg';
-//   removeBtnImg.alt = 'remove item';
-
-//   listItem.appendChild(listItemContent);
-//   removeBtn.appendChild(removeBtnImg);
-//   listItem.appendChild(removeBtn);
-//   list.appendChild(listItem);
-
-//   newTodo.addEventListener('input', () => (item.text = newTodo.value));
-// }
-
-/* <div class="list-container">
-      <ul class="list">
-        <li class="list-item">
-          <input type="checkbox">
-          <div class="list-item-content">Item 1</div>
-          <button id="remove-item"><img src="./images/icon-cross.svg" alt=""></button>
-        </li> */
